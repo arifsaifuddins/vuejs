fe1757dddce3c3530b5ffd2d043c4af177e1a13e
saifuddien <ariefsaifuddien01@gmail.com>
2022-02-26 11:58:27 +0200

    check

---
+++ src/components/advance/Router.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ key }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: "hai",
+    };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  color: blueviolet;
+}
+</style>
